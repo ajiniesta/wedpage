a82c70f26b356ec87f9eb619f5257327db0da81e
Antonio J. Iniesta <antonioj.iniesta@gmail.com>
2015-01-17 13:46:05 +0100

    Releasing a new version to heroku

--- dist/scripts/controllers/main.js
+++ dist/scripts/controllers/main.js
@@ -8,10 +8,18 @@
  * Controller of the wedpageApp
  */
 angular.module('wedpageApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', ['$log', function ($scope, $log) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+    $scope.comment = {};
+    $scope.greeting = {id : "1", content : "Here it's the content buddy"};
+
+    $scope.send = function (comment) {
+      console.log("Sending info");
+      console.log(comment); 
+      $log("Logging from object injected");
+    };
+  } ]);
